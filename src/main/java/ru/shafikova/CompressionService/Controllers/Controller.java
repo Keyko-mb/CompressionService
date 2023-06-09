0f1c5ce5d1ecb897c61279b7a99e3b6c64e4d649
Алина Шафикова <shafikova-2003@mail.ru>
2023-06-09 22:26:32 +0400

    Unfortunately, LZW algorithm refused unsuitable for my purposes.

--- src/main/java/ru/shafikova/CompressionService/Controllers/Controller.java
+++ src/main/java/ru/shafikova/CompressionService/Controllers/Controller.java
@@ -1,10 +1,12 @@
 package ru.shafikova.CompressionService.Controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.shafikova.CompressionService.Models.FileMetadata;
 import ru.shafikova.CompressionService.Services.CompressionService;
 
 import java.io.IOException;
@@ -22,12 +24,23 @@ public class Controller {
 
     @PostMapping()
     public ResponseEntity<Object> put(@RequestParam("file") MultipartFile file) throws IOException {
-        String compressedFilePath = compressionService.compress(file);
+        String originalFormat = file.getContentType();
+        String originalFileName = file.getOriginalFilename();
+        FileMetadata fileMetadata = new FileMetadata(originalFileName, originalFormat);
+
+        String compressedFilePath = compressionService.compress(file, fileMetadata);
         Path compressedFile = Path.of(compressedFilePath);
+        String compressedFileName = (originalFileName.split("\\.")[0] + "-compressed.bin").replace(" ", "_");
+        System.out.println(compressedFileName);
 
         return ResponseEntity.status(HttpStatus.OK)
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + compressedFile.getFileName())
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + compressedFileName)
                 .body(new FileSystemResource(compressedFile));
     }
-
 }
+
+
+
+
+
+
--- src/main/java/ru/shafikova/CompressionService/Services/CompressionService.java
+++ src/main/java/ru/shafikova/CompressionService/Services/CompressionService.java
@@ -2,14 +2,59 @@ package ru.shafikova.CompressionService.Services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import ru.shafikova.CompressionService.Models.FileMetadata;
 
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Service
 public class CompressionService {
-    public String compress(MultipartFile file) throws IOException {
+    public String compress(MultipartFile file, FileMetadata metadata) throws IOException {
         byte[] bytes = file.getBytes();
         Coder coder = new Coder();
-        return coder.compress(bytes);
+        List<String> codedLine = coder.compress(bytes);
+        return writeBinFile(codedLine, metadata);
+    }
+
+    public static String writeBinFile(List<String> text, FileMetadata metadata) throws IOException {
+        Path compressedFile = Files.createTempFile("compressedFile", ".bin");
+        String filePath = String.valueOf(compressedFile.toAbsolutePath());
+
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(filePath))) {
+
+            dos.writeShort(metadata.getOriginalFileName().length());
+            dos.writeChars(metadata.getOriginalFileName());
+
+            dos.writeShort(metadata.getOriginalFormat().length());
+            dos.writeChars(metadata.getOriginalFormat());
+
+            for (String s : text) {
+                int intValue = Integer.parseInt(s);
+                System.out.println("s " + s);
+                System.out.println("intValue " + intValue);
+
+//                byte msb = (byte) (intValue >> 8);
+//                byte lsb = (byte) intValue;
+//                System.out.println("msb " + msb);
+//                System.out.println("lsb " +lsb);
+//                dos.write(msb);
+//                dos.write(lsb);
+
+//                String byteString = Integer.toBinaryString(intValue);
+//                System.out.println("byteString " + byteString);
+
+                dos.writeShort(intValue);
+
+            }
+//            System.out.println("Двоичное число записано в файл.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return filePath;
     }
 }
